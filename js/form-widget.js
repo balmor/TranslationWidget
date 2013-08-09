2660d1b7109dbcfc4932e9745b6837a2765365b5
karolgorecki <kgorecki@nexway.com>
2013-08-09 13:35:42 +0200

    Updated code and added Jasmine-Helpers
    Now you can use: https://github.com/velesin/jasmine-jquery
    
    --------------------------------------------------------
    
    	new file:   SpecRunner.html
    	new file:   jasmine/jasmine-jquery.js
    	modified:   jasmine/spec/widgetSpec.js
    	modified:   js/form-widget.js
    	modified:   less/main.less

--- js/form-widget.js
+++ js/form-widget.js
@@ -1,25 +1,14 @@
-/*
-* jQuery Form Widget to translate
-*
-* @author Ariana Las <ariana.las@gmail.com>
-*
-*/
+/**
+ * jQuery Form Widget to translate
+ *
+ * @author Ariana Las <ariana.las@gmail.com>
+ *
+ */
 
 
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
 
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
-
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
-
-    // Create the defaults once
+    /** @constructor */
     var pluginName = "formWidget",
         defaults = {
             inputNamePrefix: "translateWidget-"
@@ -31,12 +20,7 @@
         this.countWidgetInstances = countWidgetInstances;
         $thisElement = $(this.element);
 
-        // jQuery has an extend method which merges the contents of two or
-        // more objects, storing the result in the first object. The first object
-        // is generally empty as we don't want to alter the default options for
-        // future instances of the plugin
         this.options = $.extend( {}, defaults, options );
-
         this._defaults = defaults;
         this._name = pluginName;
 
@@ -89,6 +73,9 @@
             });
         },
 
+        /**
+         *  Set the unique input name. User can set default prefix name.
+         */
         setInputName: function() {
             $thisElement.attr("name",this.options.inputNamePrefix + this.countWidgetInstances); 
         },
@@ -315,8 +302,6 @@
 
     };
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
     $.fn[pluginName] = function ( options ) {
         var countWidgetInstances = 0;
 
