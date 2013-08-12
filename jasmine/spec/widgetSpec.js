c3bc6505ef0f881f08abf3ad9abee7c58f7a6a60
karolgorecki <kgorecki@nexway.com>
2013-08-12 21:44:40 +0200

    Updated tests and widget code.
    	modified:   SpecRunner.html
    	modified:   index.html
    	modified:   jasmine/spec/widgetSpec.js
    	modified:   js/form-widget.js

--- jasmine/spec/widgetSpec.js
+++ jasmine/spec/widgetSpec.js
@@ -1,9 +1,9 @@
-describe("Translation widget", function() {
+describe("Translation fields widget", function() {
 
     beforeEach(function(){
         $container = $('<div class="control-group"><label class="control-label">Translation</label><div class="controls"></div></div>').appendTo('form');
         $base = $('<input type="text" class="m-wrap lang-translation" name="test" disabled />').appendTo($container.children(".controls"));
-        $wi = $base.formWidget();
+        $wi = $base.translationFields();
     });
 
     afterEach(function() {
@@ -29,6 +29,50 @@ describe("Translation widget", function() {
             var openTranslation = $base.prev('.open-translation').trigger("click");
             expect(openTranslation).toHaveClass("open");
             expect(openTranslation.parent()).toHaveClass("show");
+        });
+    });
+
+    describe("body on click when translation popup is showed", function() {
+
+        it("should hide it", function() {
+            var openTranslation = $base.prev('.open-translation').trigger("click");
+            $("body").trigger("click");
+            console.log($base.parent());
+            expect($base.parent()).not.toHaveClass("show");
+        });
+    });
+
+    describe("the apply button", function() {
+
+        it("after click should add new badge", function() {
+            var openTranslation = $base.prev('.open-translation').trigger("click");
+
+            // add polish translation
+            $base.parent().find(".select-language").val("PL").change(); //select PL
+            $base.parent().find(".m-wrap.new-word").val("Tekst po polsku"); // add translation
+            $base.parent().find(".apply").trigger("click"); // click the button
+
+            var languageTabsContainer = $base.parent().find(".language-tabs");
+            expect(languageTabsContainer).toContain('span#PL>input[value="Tekst po polsku"]');
+        });
+    });
+
+    describe("the remove badge button", function() {
+
+        it("after click should remove it", function() {
+            var openTranslation = $base.prev('.open-translation').trigger("click");
+
+            // add polish translation
+            $base.parent().find(".select-language").val("PL").change(); //select PL
+            $base.parent().find(".m-wrap.new-word").val("Tekst po polsku"); // add translation
+            $base.parent().find(".apply").trigger("click"); // click the button
+
+            $wi.data("plugin_translationFields").options.removeText = "PRESS OK";
+            var removeBadge = $base.parent().find(".remove");
+                removeBadge.trigger("click");
+
+            var languageTabsContainer = $base.parent().find(".language-tabs");
+            expect(languageTabsContainer).toBeEmpty();
         });
     });
 });
